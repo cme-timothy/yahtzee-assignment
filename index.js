c492c08f069f5a0f1f6ed50246cc948502541925
cme-timothy <timothy.magee@cmeducations.se>
2021-12-07 00:18:22 +0100

    foundation of the game is done

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function turnRollDice() {
     function rollDiceFirstTime() {
     const input = prompt("To roll dice type in yes");
         if (input === "") {
-            fiveDice.diceOne = 1;
-            fiveDice.diceTwo = 2;
+            fiveDice.diceOne = 2;
+            fiveDice.diceTwo = 4;
             fiveDice.diceThree = 3;
-            fiveDice.diceFour = 4;
-            fiveDice.diceFive = 5;
+            fiveDice.diceFour = 3;
+            fiveDice.diceFive = 1;
         }
     }
 
@@ -102,47 +102,33 @@ let lowerSection = {
     yahtzee: 0,
 }
 
-/* pair or two pairs */
+/* pair or two pairs + three or more of a kind + full house */
 let dicePairArray = [];
-function getDicePair(number) {
-    let diceSameNumber = fiveDiceArray.filter(element => element === number);
-    if (diceSameNumber.length === 2) {
-        return dicePairArray.push(diceSameNumber);
-    }
-}
-
-for (let i = 0; i < 6; i++) {
-    getDicePair(i);
-}
-
-console.log(dicePairArray);
-
-if (dicePairArray.length === 2 || dicePairArray.length === 0) {
-    lowerSection.pair = sumOfDice(lowerSection.pair);
-    lowerSection.twoPairs = sumOfDice(lowerSection.twoPairs);
-} else if (dicePairArray.length === 1 || dicePairArray.length === 0) {
-    lowerSection.pair = sumOfDice(lowerSection.pair);
-}
-
-/* three or more of a kind + full house */
 let fullHouseThreeOfAKind = false;
 let fullHouseTwoOfAKind = false;
 function diceTwoOrMoreOfAKind(number) {
 let diceSameNumber = fiveDiceArray.filter(element => element === number);
     if (diceSameNumber.length === 3) {
+        dicePairArray.push(diceSameNumber);
+        lowerSection.pair = sumOfDice(lowerSection.pair);
         fullHouseThreeOfAKind = true;
         return lowerSection.threeOfAKind = sumOfDice(lowerSection.threeOfAKind);
 
     } else if (diceSameNumber.length === 4) {
+        lowerSection.pair = sumOfDice(lowerSection.pair);
+        lowerSection.twoPairs = sumOfDice(lowerSection.twoPairs);
         lowerSection.threeOfAKind = sumOfDice(lowerSection.threeOfAKind);
         return lowerSection.fourOfAKind = sumOfDice(lowerSection.fourOfAKind);
 
     } else if (diceSameNumber.length === 5) {
+        lowerSection.pair = sumOfDice(lowerSection.pair);
+        lowerSection.twoPairs = sumOfDice(lowerSection.twoPairs);
         lowerSection.threeOfAKind = sumOfDice(lowerSection.threeOfAKind);
         lowerSection.fourOfAKind = sumOfDice(lowerSection.fourOfAKind);
         return lowerSection.yahtzee = sumOfDice(lowerSection.yahtzee);
 
     } else if (diceSameNumber.length === 2) {
+        dicePairArray.push(diceSameNumber);
         return fullHouseTwoOfAKind = true;
     }
 }
@@ -151,16 +137,29 @@ for (let i = 0; i < 6; i++) {
     diceTwoOrMoreOfAKind(i);
 }
 
+if (dicePairArray.length === 2) {
+    lowerSection.pair = sumOfDice(lowerSection.pair);
+    lowerSection.twoPairs = sumOfDice(lowerSection.twoPairs);
+} else if (dicePairArray.length === 1) {
+    lowerSection.pair = sumOfDice(lowerSection.pair);
+}
+
 if (fullHouseThreeOfAKind && fullHouseTwoOfAKind) {
     lowerSection.fullHouse = sumOfDice(lowerSection.fullHouse);
 }
 
 /* low and high straight */
-    if (dicePairArray.length !== 1 && dicePairArray.length !== 0) {
-        lowerSection.lowStraight = sumOfDice(lowerSection.lowStraight);
-        lowerSection.highStraight = sumOfDice(lowerSection.highStraight);
-    } else if (dicePairArray.length !== 2 && dicePairArray.length !== 0) {
-        lowerSection.lowStraight = sumOfDice(lowerSection.lowStraight);
-    }
+if (dicePairArray.length !== 2 && dicePairArray.length !== 0) {
+    lowerSection.lowStraight = sumOfDice(lowerSection.lowStraight);
+}
+
+const highStraightSix = "2,3,4,5,6";
+const highStraightOne = "1,2,3,4,5";
+fiveDiceArray.sort(function(a, b){return a-b});
+const fiveDiceString = fiveDiceArray.toString();
+if (fiveDiceString === highStraightSix || fiveDiceString === highStraightOne) {
+    lowerSection.lowStraight = sumOfDice(lowerSection.lowStraight);
+    lowerSection.highStraight = sumOfDice(lowerSection.highStraight);
+}
 
 console.log(lowerSection);
